e724fed87482c563559d5888fd5dc98de3a4a5ee
Aditya <adityasahay44@gmail.com>
2026-05-05 13:10:32 +0530

    Added User model class

---
+++ src/main/java/com/aditya/authsystem/model/User.java
@@ -0,0 +1,12 @@
+package com.aditya.authsystem.model;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    // getters and setters
+}
