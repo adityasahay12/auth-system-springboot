3ada4f951ffbac75a9bf3dd421d4b7246d1349d7
Aditya <adityasahay44@gmail.com>
2026-05-05 13:19:07 +0530

    Converted User model to JPA entity

--- src/main/java/com/aditya/authsystem/model/User.java
+++ src/main/java/com/aditya/authsystem/model/User.java
@@ -1,8 +1,15 @@
 package com.aditya.authsystem.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String email;
     private String password;
