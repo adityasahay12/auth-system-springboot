17bfd821b7ee3b18a345c76330db653dae6aa238
Aditya <adityasahay44@gmail.com>
2026-05-05 13:14:29 +0530

    Added authentication API endpoints

--- src/main/java/com/aditya/authsystem/controller/AuthController.java
+++ src/main/java/com/aditya/authsystem/controller/AuthController.java
@@ -1,4 +1,23 @@
 package com.aditya.authsystem.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @PostMapping("/register")
+    public String registerUser() {
+        return "User registered successfully";
+    }
+
+    @PostMapping("/login")
+    public String loginUser() {
+        return "User logged in successfully";
+    }
+
+    @GetMapping("/test")
+    public String testApi() {
+        return "Protected API working";
+    }
 }
