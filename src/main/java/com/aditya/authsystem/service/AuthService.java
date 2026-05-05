e631549d5b9ea1a1b9e95bda070192f02c136f24
Aditya <adityasahay44@gmail.com>
2026-05-05 13:24:18 +0530

    Connected service with repository and password encryption

--- src/main/java/com/aditya/authsystem/service/AuthService.java
+++ src/main/java/com/aditya/authsystem/service/AuthService.java
@@ -1,12 +1,27 @@
 package com.aditya.authsystem.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.aditya.authsystem.repository.UserRepository;
+import com.aditya.authsystem.model.User;
+
+@Service
 public class AuthService {
 
-    public String register() {
-        return "Register logic here";
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public String register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User registered successfully";
     }
 
-    public String login() {
-        return "Login logic here";
+    public String login(String email, String password) {
+        return "Login logic will be implemented with JWT";
     }
 }
