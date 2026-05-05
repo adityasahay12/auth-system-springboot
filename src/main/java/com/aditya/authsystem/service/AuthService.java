0ab1bd27a7ff673b77593fdeaa5952eeb34fed0c
Aditya <adityasahay44@gmail.com>
2026-05-05 13:15:16 +0530

    Added AuthService layer

---
+++ src/main/java/com/aditya/authsystem/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.aditya.authsystem.service;
+
+public class AuthService {
+
+    public String register() {
+        return "Register logic here";
+    }
+
+    public String login() {
+        return "Login logic here";
+    }
+}
